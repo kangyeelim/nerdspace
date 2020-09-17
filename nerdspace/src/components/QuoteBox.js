caf781e22b6084c709eba83bbc77f05557533597
Yen-Peng <e0318790@u.nus.edu>
2020-09-18 03:35:41 +0800

    Change quotebox to fetch quotes from API instead of local database

--- nerdspace/src/components/QuoteBox.js
+++ nerdspace/src/components/QuoteBox.js
@@ -1,48 +1,56 @@
 import React from "react";
-import QuoteAndAuthor from "./QuoteAndAuthor";
-import quotes from "./QuoteDatabase";
 import "./QuoteBox.css";
+import { Button, Card, CardContent, CardActions } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
 /**
  * QuoteBox handles the quote button click and 
- * randomizes the quotes from QuoteDatabase and the colours displayed
+ * fetches a random quote from random quote generator API
  */
 
+const config = {
+    apiUrl: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+}
+
 class QuoteBox extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
         this.state = {
-            quote: quotes[0].quote,
-            author: quotes[0].author,
-        };
+            quoteData: [],
+            quote: '',
+            author: ''
+        }
+        this.randomQuote = this.randomQuote.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
-        const newQuote = this.randomQuote();
-        this.setState({
-            quote: newQuote.quote,
-            author: newQuote.author,
-        });
-        this.shuffleQuotes(quotes);
+        fetch(config.apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    quoteData: data.quotes
+                }, () => {
+                    // Generates a random quote on initial load
+                    this.handleClick();
+                })
+            })
+            .catch(error => console.log('Error', error));
     }
 
     randomQuote() {
-        const randomNumber = Math.floor(Math.random() * quotes.length);
-        return quotes[randomNumber];
+        const randomNumber = Math.floor(Math.random() * this.state.quoteData.length);
+        return this.state.quoteData[randomNumber];
     }
 
-    shuffleQuotes(array) {
-        return array.sort(() => Math.random() - 0.5)
-    }
-
-    handleClick = () => {
+    handleClick() {
         const newQuote = this.randomQuote();
         this.setState({
             quote: newQuote.quote,
             author: newQuote.author
-        });
-        this.shuffleQuotes(quotes)
-    };
+        })
+    }
 
     randomColor() {
         const color = `rgb(
@@ -53,16 +61,48 @@ class QuoteBox extends React.Component {
     }
 
     render() {
+        const randomColor = this.randomColor();
         return (
-            <div>
-                <QuoteAndAuthor
-                    displayColor={this.randomColor}
-                    handleClick={this.handleClick}
-                    {...this.state}
-                />
-            </div>
-        );
+            <Card className="quoteBox">
+                <CardContent>
+                <div className="fadeIn" key={Math.random()} style={{ color: randomColor }}>
+                        <Typography variant="h6" style={styles.quote}>
+                    "{this.state.quote}"
+                </Typography>
+                <Typography variant="body2" style={styles.author}>
+                    - {this.state.author} -
+                </Typography>
+                </div>
+                </CardContent>
+                <CardActions style={{ justifyContent: "flex-end" }}>
+                    <Button
+                        variant="contained"
+                        style={{
+                            backgroundColor: randomColor,
+                            color: "white",
+                            marginRight: "20px",
+                            marginBottom: "20px",
+                            borderRadius: "5px",
+                        }}
+                        onClick={this.handleClick}
+                    >
+                        New quote
+                    </Button>
+                </CardActions>
+            </Card>
+        )
     }
 }
+
+const styles = {
+    quote: {
+        margin: "10px",
+        textAlign: "left",
+    },
+    author: {
+        margin: "10px",
+        fontStyle: "italic",
+    }
+};
 
 export default QuoteBox;
