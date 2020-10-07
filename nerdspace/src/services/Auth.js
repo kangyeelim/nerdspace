7e09842d23149c53972e388548a7538c1aa7d923
Lim Kang Yee <kangyee2408@gmail.com>
2020-10-07 12:47:16 +0800

    Updated Auth to check if user logged in and if room accessible to user.

--- nerdspace/src/services/Auth.js
+++ nerdspace/src/services/Auth.js
@@ -1,10 +1,29 @@
 import axios from 'axios';
+import { USER_API_URL, ROOM_API_URL } from './Urls';
 
 export async function isRoomAccessibleToUser(googleID, roomID) {
-  var res = await axios.get(`http://localhost:5000/studyrooms/byRoomID/${roomID}`);
-  var members = Object.values(await res.data.data.members);
-  var res2 = await axios.get(`http://localhost:5000/users/byGoogleID/${googleID}`);
-  var user = (await res2).data.data[0];
-  var rooms = Object.values(user.rooms);
-  return members.includes(googleID) && rooms.includes(roomID);
+  try {
+    var res = await axios.get(ROOM_API_URL + `/byRoomID/${roomID}`);
+    var members = Object.values(await res.data.data.members);
+    var res2 = await axios.get(USER_API_URL + `/byGoogleID/${googleID}`);
+    var user = (await res2).data.data[0];
+    var rooms = Object.values(user.rooms);
+    return members.includes(googleID) && rooms.includes(roomID);
+  } catch (error) {
+    return false;
+  }
+}
+
+export async function isUserLoggedIn(googleID) {
+  try {
+    var res = await axios.get(USER_API_URL + `/byGoogleID/${googleID}`);
+    if ((await res).data.message == 'GET success') {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    return false;
+  }
+
 }
