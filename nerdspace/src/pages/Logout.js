9062c92400f30edc924211f46d7f68174301d558
Yen-Peng <e0318790@u.nus.edu>
2020-11-08 10:50:20 +0800

    Improve logout ui

--- nerdspace/src/pages/Logout.js
+++ nerdspace/src/pages/Logout.js
@@ -2,49 +2,53 @@ import React from "react";
 import { connect } from "react-redux";
 import { deleteProfile, deleteToken } from "../redux/actions";
 import { deleteTokenFromDB } from "../services/Auth";
-import { Row, Col, Container } from "react-bootstrap";
-import { Button, Link, Card, CardContent } from "@material-ui/core";
+import { Button, Link, Card, CardContent, Typography } from "@material-ui/core";
 
 class Logout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleLogout = this.handleLogout.bind(this);
-  }
-  handleLogout() {
-    this.props.deleteToken();
-    this.props.deleteProfile();
-    deleteTokenFromDB(this.props.token);
-    this.props.history.push("/");
-  }
-  render() {
-    return (
-      <div>
-        <Container>
-          <Row className="justify-content-md-center">
-            <Col md={6}>
-              <Card>
-                <CardContent>
-                  <hr></hr>
-                  Logout from Nerdspace?
-                  <Link exact to="/login">
-                    <Button
-                      size="sm"
-                      className="float-right"
-                      color="secondary"
-                      onClick={this.handleLogout}
-                    >
-                      Logout
-                    </Button>
-                  </Link>
-                </CardContent>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+    handleLogout() {
+        this.props.deleteToken();
+        this.props.deleteProfile();
+        deleteTokenFromDB(this.props.token);
+        this.props.history.push("/");
+    }
+    render() {
+        return (
+            <div style={styles.container}>
+                <Card>
+                    <CardContent>
+                        <Typography variant="h6">Logout from Nerdspace</Typography>
+                        <hr></hr>
+                        <Link exact to="/login">
+                            <Button
+                                size="sm"
+                                className="center"
+                                color="primary"
+                                onClick={this.handleLogout}
+                            >
+                                Logout
+                            </Button>
+                        </Link>
+                    </CardContent>
+                </Card>
+            </div>
+        );
+    }
 }
+
+const styles = {
+    container: {
+        marginTop: "30vh",
+        display: "flex",
+        alignItems: "center",
+        textAlign: "center",
+        justifyContent: "center",
+    }
+}
+
 
 const mapStateToProps = (state) => {
   return {
